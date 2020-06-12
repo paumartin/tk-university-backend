02acd9f922eb7e0c8e2d78085d376be18e09d9b2
Pau Martin <pau.martin@travelperk.com>
2020-06-12 17:42:42 +0200

    Adds models and serializers

---
+++ app/recipe/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+
+class Ingredient(models.Model):
+    """Ingredient object"""
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+
+    def __str__(self):
+        return self.name
---
+++ app/recipe/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+
+from .models import Recipe, Ingredient
+
+
+class IngredientSerializer(ModelSerializer):
+    """Serializer for the ingredient object"""
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+        read_only_fields = fields
+
+
+class RecipeSerializer(ModelSerializer):
+    """Serializer for the recipe object"""
+
+    ingredients = IngredientSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'description', 'ingredients',)
+        read_only_fields = ('id', 'ingredients',)
